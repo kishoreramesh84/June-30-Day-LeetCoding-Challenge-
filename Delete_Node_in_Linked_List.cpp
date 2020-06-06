0189684d9872a9f1b3cd4972e52e80a251a0b652
kishoreramesh84 <kishoreramesh84@yahoo.com>
2020-06-07 01:36:02 +0530

    Day 2 Delete Node in Linked List Solution

--- Delete_Node_in_Linked_List.cpp
+++ Delete_Node_in_Linked_List.cpp
@@ -13,7 +13,7 @@
 3) Now change the next link of given node as next node link(node->next=node->next->next).
 4) Delete the temporary node.
 
-	The above algorithm cannot be used when the given node is the last node.
+	Note: The above algorithm cannot be used when the given node is the last node.
 	Time Complexity: O(1)
 	Problem Link: https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3348/
 */
