c051b53ad0b6465cb6e2002e47eb041cbedc5d9d
kishoreramesh84 <kishoreramesh84@yahoo.com>
2020-06-09 23:22:01 +0530

    Day 9 Is Subsequence Solution

---
+++ Is_Subsequence.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i,j=0;
+        for(i=0;i<t.size();i++)
+        {
+            if(j<s.size() and s[j]==t[i])
+            {
+                j++;
+            }
+        }
+        return j==s.size();
+    }
+};
+
+/* Problem Link: https://leetcode.com/explore/challenge/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3355/
+   Time Complexity: O(max(len(s),len(t))) => O(N)
+   Space Complexity: O(1)
+*/
